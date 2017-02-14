9391d7f6abe646bb72e3ee1ffcb59702737e0947
Paige Niedringhaus <paige_niedringhaus@homedepot.com>
2017-02-13 20:27:40 -0500

    commit four

--- coinFlip.jsx
+++ coinFlip.jsx
@@ -1,32 +1,46 @@
 
 var Coin = React.createClass({
 		sides: [
-        'http://www.marshu.com/articles/images-website/articles/presidents-on-coins/half-dollar-coin-tail.jpg',
-        'http://www.marshu.com/articles/images-website/articles/presidents-on-coins/quarter-coin-head.jpg'
+        	'http://www.marshu.com/articles/images-website/articles/presidents-on-coins/half-dollar-coin-tail.jpg',
+        	'http://www.marshu.com/articles/images-website/articles/presidents-on-coins/quarter-coin-head.jpg'
         ],
-        flipCoin: function() {
-        	console.log("flip coin");
+
+        getInitialState: function() {
+        	
+        	return{
+        		image: this.sides[0]
+        	};
         },
+
+        flipCoin: function() {
+        	var randomSide = Math.round(Math.random());
+        	console.log(randomSide);
+        	
+        	this.setState({
+        		image: this.sides[randomSide]
+        		});
+        },
+
         render: function() {
         	console.log(this.sides);
         	return(
         		<div>
         			<button onClick={this.flipCoin}>Flip Coin!</button>
-        			<h1>Coin goes here</h1>
+        			<img src={this.state.image} />
         		</div>
-        		)
+        		);
 		}
-})
+});
 
 var App = React.createClass({
 	render: function() {
 		return(
 			<Coin />
-		)
+		);
 	}
 });
 
 ReactDOM.render(
 	<App />,
 	document.getElementById('container')
-)
+);
